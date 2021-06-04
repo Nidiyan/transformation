b8385d2eabb04e8cd78abf0ceadcf8a256c40f66
BenDavis71 <69690328+BenDavis71@users.noreply.github.com>
2021-06-04 13:40:51 -0400

    Update multiplyExample.py

--- transformation/scripts/multiplyExample.py
+++ transformation/scripts/multiplyExample.py
@@ -1,21 +1,24 @@
 import pandas as pd 
 import numpy as np 
 import argparse
-
-from os import getcwd
-
+ 
+import os
+ 
 # NOTE: FIGURE OUT HOW TO SAVE IN THE PROPER LOCATION -> SHOULD BE SAVED TO /GENERATED FILES
 # CAN PASS COMPLETE PATH TO BOTH INPUT AND OUTPUT XL FILE
-
-initialPath = getcwd() + "/"
-
+ 
 parser = argparse.ArgumentParser()
-parser.add_argument('pathToFile', help='File to read in')
+parser.add_argument('pathToFile', help='Path to File')
+parser.add_argument('fileName', help='Name of File')
 args = parser.parse_args()
 pathToFile = args.pathToFile
-
-df = pd.read_excel(pathToFile, engine='openpyxl')
-
+fileName = args.fileName
+ 
+fullPath = os.path.join(pathToFile,'uploadedFiles',fileName)
+fullPathDownload = os.path.join(pathToFile,'generatedFiles', 'Output-' + fileName)
+ 
+df = pd.read_excel(fullPath, engine='openpyxl')
+ 
 df['product'] = df['num1'] * df['num2']
-
-df.to_excel(initialPath + 'generatedFiles/multiply.xlsx', index=False)
+ 
+df.to_excel(fullPathDownload, index=False)
