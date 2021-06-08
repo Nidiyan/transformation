5a4c1d4f682c436ea92b1f86d7ffbdb6625d5c23
root <root@DESKTOP-CIPN87P.localdomain>
2021-06-08 09:44:37 -0400

    React installed; App component created and includes react-bootstrap

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div>
+            <h1>
+                Hello, World!
+            </h1>
+            <Button>Test</Button>
+        </div>
+    );
+}
+
+export default App;
